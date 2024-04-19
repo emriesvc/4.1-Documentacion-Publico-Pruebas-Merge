63d07bf89a8093ef09160a88ad135f319eaf8d3e
“rpallin0705” <rpalomareslinares@gmail.com>
2024-04-19 14:21:11 +0000

    Documentacion clase  Producto y constructor

--- calculadora/Producto.java
+++ calculadora/Producto.java
@@ -1,9 +1,20 @@
 package calculadora;
 
-
+/**
+ * Clase producto que se utiliza para realizar operaciones aritmétricas de multiplicación
+ * @author Rafa
+ * @version 1.0
+ * @since 1.0
+ */
 public class Producto {
 
-	
+	/**
+	 * Función que recibe como parámetros dos números reales y devuelve su producto
+	 * @param a primer producto real
+	 * @param b segundo producto real
+	 * @return el resultado del producto de los dos números
+	 * @version 1.0
+	 */
 	public float dosReales(float a, float b) {
 		return a*b;
 	}
